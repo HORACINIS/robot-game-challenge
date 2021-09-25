1f294648caeb926f54c2bff331e18b1335122469
Horacinis <horacinis@gmail.com>
2021-09-25 14:56:36 +1000

    Placed robot in the origin (0,0) for testing

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 console.log('Conected')
 
+const robot = document.createElement('img');
+robot.src = './robot/robot_right.png'; //robot facing will be dynamic
+
+// document.querySelector('form').appendChild(robot)
+
+
+let tableUnits = document.querySelectorAll('.unit')
+console.log(tableUnits)
+tableUnits[20].appendChild(robot);
