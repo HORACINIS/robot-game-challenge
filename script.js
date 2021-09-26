e04faa70758d64b12e51cba8275493ee9ee77648
Horacinis <horacinis@gmail.com>
2021-09-26 18:41:40 +1000

    Created the moveRobot function, there are still a few things that need to be done and also, refactoring

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 'use strict'
 
 
-
-
+const tableSurface = document.querySelector('#table-surface');
 
 const robot = document.createElement('img');
 robot.src = './robot/robot_right.png'; //robot facing will be dynamic
-
-// document.querySelector('form').appendChild(robot)
-
 
 
 
 // creating 2d array
 
-const myArray = [];
-const rows = 0;
-const cols = 5;
+function createTabletop() {
+  const tableTopDimensionUnits = [];
+  // const rows = 0;
+  // const cols = 5;
 
-for (let r = 4; r >= rows; r--) {
-  myArray[r] = [];
-  for (let c = 0; c < cols; c++) {
-    myArray[r][c] = `${c}, ${r}`;
-    let surface = document.querySelector('#table-surface')
-    let unit = document.createElement('div');
-    unit.classList = 'unit';
-    unit.id = myArray[r][c];
-    surface.appendChild(unit);
-    unit.textContent = myArray[r][c]
+  for (let r = 4; r >= 0; r--) {
+    tableTopDimensionUnits[r] = [];
+    for (let c = 0; c < 5; c++) {
+      const unit = document.createElement('div');
 
-    // console.log(surface)
-    // console.log(myArray[r][c])
+      // tableTopDimensionUnits[r][c] = `${c}, ${r}`;
+      tableTopDimensionUnits[r][c] = new Array(`${[c, r]}`);
 
-    // console.log(c)
-    // console.log(r)
+      unit.classList = 'unit';
+      unit.id = tableTopDimensionUnits[r][c];
+      tableSurface.appendChild(unit);
+      unit.textContent = tableTopDimensionUnits[r][c]
+    }
   }
+  console.log(tableTopDimensionUnits);
 }
-console.log(myArray);
+createTabletop();
 
 
 
@@ -44,6 +39,29 @@ console.log(myArray);
 
 
 
+
+// THIS WILL ATTACH THE ROBOT 
+
+let tableUnits = document.querySelectorAll('.unit')
+console.log(tableUnits[20].id)
+
+
+tableUnits[20].appendChild(robot);
+
+// tableUnits.forEach(i => console.log(i))
+
+const moveRobot = (x, y) => {
+  tableUnits.forEach(divElement => {
+    if (divElement.id === `${x},${y}`) {
+      console.log('YES!!')
+      console.log(divElement)
+      divElement.appendChild(robot)
+    } else {
+      console.log('no')
+    }
+  });
+};
+moveRobot(4, 3);
 
 
 
