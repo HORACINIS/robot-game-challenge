cf6d3e034f34dba4b4f5bb7897e2a7b5878187d3
Horacinis <horacinis@gmail.com>
2021-09-26 15:28:23 +1000

    Removed extra code and recreated the table grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
-console.log('Conected')
+
+
+
 
 const robot = document.createElement('img');
 robot.src = './robot/robot_right.png'; //robot facing will be dynamic
@@ -8,6 +10,46 @@ robot.src = './robot/robot_right.png'; //robot facing will be dynamic
 // document.querySelector('form').appendChild(robot)
 
 
-let tableUnits = document.querySelectorAll('.unit')
-console.log(tableUnits)
-tableUnits[20].appendChild(robot);
+
+
+// creating 2d array
+
+const myArray = [];
+const rows = 0;
+const cols = 5;
+
+for (let r = 4; r >= rows; r--) {
+  myArray[r] = [];
+  for (let c = 0; c < cols; c++) {
+    myArray[r][c] = `${c}, ${r}`;
+    let surface = document.querySelector('#table-surface')
+    let unit = document.createElement('div');
+    unit.classList = 'unit';
+    unit.id = myArray[r][c];
+    surface.appendChild(unit);
+    unit.textContent = myArray[r][c]
+
+    // console.log(surface)
+    // console.log(myArray[r][c])
+
+    // console.log(c)
+    // console.log(r)
+  }
+}
+console.log(myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
