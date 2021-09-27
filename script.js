be16e381fe5f316de544ad9de313797902fc743e
Horacinis <horacinis@gmail.com>
2021-09-27 17:32:02 +1000

    Now the robot rotates left and right as intented, did a bit of code refactoring

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-'use strict'
+'use strict';
 
-const tableSurface = document.querySelector('#table-surface');
+
 
 // creating 2d array
-
-function createTabletop() {
+const createTabletop = () => {
+  const tableSurface = document.querySelector('#table-surface');
   const tableTopDimensionUnits = [];
-  // const rows = 0;
-  // const cols = 5;
-
   for (let r = 4; r >= 0; r--) {
     tableTopDimensionUnits[r] = [];
     for (let c = 0; c < 5; c++) {
@@ -28,71 +25,138 @@ function createTabletop() {
 createTabletop();
 
 
-
-
-// THIS WILL ATTACH THE ROBOT 
-
-let tableUnits = document.querySelectorAll('.unit')
-// console.log(tableUnits[20].id)
-
-
-// tableUnits[20].appendChild(robot);
-
-
-
 class Robot {
   constructor() {
     this.robot = document.createElement('img');
-    this.degrees = 0;
+    // this.degrees = 0;
+    this.facingDirectionsOptions = ['SOUTH', 'WEST', 'NORTH', 'EAST']; //CLOCKWISE (RIGHT)
+    this.degreesOptions = [0, 90, 180, 270];
   };
 
   place(x, y, f) {
     this.robot.src = './robot/robot.png';
     this.x = x;
     this.y = y;
-    this.coordinatesGiven = `${this.x},${this.y}`;
-    tableUnits.forEach(element => {
-      if (element.id === this.coordinatesGiven) {
-        element.appendChild(this.robot)
-      }
+    this.f = f;
+    this.degrees = 0;
+    this.coordinates = `${this.x},${this.y}`;
+    document.querySelectorAll('.unit').forEach(dimensionUnit => {
+      if (dimensionUnit.id === this.coordinates) dimensionUnit.appendChild(this.robot);
     });
-    switch (f) {
-      case 'NORTH':
-        this.degrees = 180;
+
+    switch (this.f) {
+      case this.facingDirectionsOptions[0]: // SOUTH (0 DEGREES - ALWAYS)
+        this.degrees = this.degreesOptions[0];
+        // this.f = this.facingDirectionsOptions[0]
         break;
-      case 'SOUTH':
-        this.degrees = 0;
+      case this.facingDirectionsOptions[1]: // WEST (90 DEGREES - ALWAYS)
+        this.degrees = this.degreesOptions[1];
+        // this.f = this.facingDirectionsOptions[1]
         break;
-      case 'EAST':
-        this.degrees = 270;
+      case this.facingDirectionsOptions[2]: // NORTH (180 DEGREES - ALWAYS)
+        this.degrees = this.degreesOptions[2];
+        // this.f = this.facingDirectionsOptions[3]
         break;
-      case 'WEST':
-        this.degrees = -270;
+      case this.facingDirectionsOptions[3]: // EAST (270 DEGREES - ALWAYS)
+        this.degrees = this.degreesOptions[3];
+        // this.f = this.facingDirectionsOptions[2]
         break;
     }
     this.robot.style.transform = `rotate(${this.degrees}deg)`
+    console.log('this.f coming from place: ', this.f)
   }
-  rotateLeft() {
-    this.degrees -= 90;
+
+
+
+  left() {
+    let index = this.facingDirectionsOptions.indexOf(this.f);
+    const filtering = this.facingDirectionsOptions.find(filter => filter === this.f)
+    console.log(index)
+    index -= 1;
+    if (index < 0) index = 3;
+
+
+    this.f = this.facingDirectionsOptions[index]
+    this.robot.style.transform = `rotate(${this.degreesOptions[index]}deg)`;
+
+    console.log('------------- LEFT() ------------')
+    console.log('index ', index)
+    console.log(this.degrees + ' degrees')
+    console.log(this.f)
+    console.log('filtering ', filtering)
+    console.log('facing direction ', this.facingDirectionsOptions[index])
+    // console.log('cordinadas left() ', this.coordinates)
+    console.log('**********************************')
+
+  }
+
+  right() {
+    let index = this.facingDirectionsOptions.indexOf(this.f);
+    const filtering = this.facingDirectionsOptions.find(filter => filter === this.f)
+    console.log(index)
+    index += 1;
+    if (index > 3) index = 0;
+
+
+    this.f = this.facingDirectionsOptions[index]
+    this.robot.style.transform = `rotate(${this.degreesOptions[index]}deg)`;
+
+    console.log('------------- RIGHT() ------------')
+    console.log('index ', index)
+    console.log(this.degrees + ' degrees')
+    console.log(this.f)
+    console.log('filtering ', filtering)
+    console.log('facing direction ', this.facingDirectionsOptions[index])
+    // console.log('cordinadas right() ', this.coordinates)
+    console.log('**********************************')
+
+  }
+
+  move() {
+    console.log('------------- MOVE() Before ------------')
+    console.log(this.x, this.y)
     console.log(this.degrees)
-    this.robot.style.transform = `rotate(${this.degrees}deg)`;
+    console.log(this.f)
+    console.log('*****************************************')
+    if (this.f === 'NORTH') {
+      this.y += 1;
+      document.querySelector(`[id='${this.x},${this.y}']`).appendChild(this.robot);
+    }
+    if (this.f === 'SOUTH') {
+      this.y -= 1;
+      document.querySelector(`[id='${this.x},${this.y}']`).appendChild(this.robot);
+    }
+    if (this.f === 'EAST') {
+      this.x += 1;
+      document.querySelector(`[id='${this.x},${this.y}']`).appendChild(this.robot);
+    }
+    if (this.f === 'WEST') {
+      this.x -= 1;
+      document.querySelector(`[id='${this.x},${this.y}']`).appendChild(this.robot);
+    }
+    console.log('------------- MOVE() After ------------')
+    console.log(this.x, this.y)
+    console.log(this.degrees + ' degrees')
+    console.log(this.f)
+    console.log('****************************************')
   }
-  rotateRight() {
-    this.degrees += 90;
-    this.robot.style.transform = `rotate(${this.degrees}deg)`;
+  report() {
+    console.log(`Current position X${this.x}, Y${this.y}, ${this.f}`);
   }
 }
 
-const robotOne = new Robot();
-robotOne.place(4, 3, 'EAST');
-// robotOne.rotateLeft();
-// robotOne.rotateLeft();
-// robotOne.rotateRight();
-// robotOne.rotateLeft();
+const robot1 = new Robot();
+robot1.place(2, 3, 'NORTH');
+robot1.move()
+robot1.report()
+robot1.right()
+robot1.report()
+robot1.right()
 
-
-// const robotTwo = new Robot();
-// robotTwo.place(0, 3, 'EAST');
+// const robot2 = new Robot();
+// robot2.place(0, 3, 'EAST');
+// robot2.move()
+// robot1.report()
 
 
 
